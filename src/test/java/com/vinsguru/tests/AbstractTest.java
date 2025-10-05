6d160f2402249ec1807afd9c080df730c07a96c4
HarshitBhadani <harshitbhadani62@gmail.com>
2025-10-05 15:42:18 +0530

    Token Removed

--- src/test/java/com/vinsguru/tests/AbstractTest.java
+++ src/test/java/com/vinsguru/tests/AbstractTest.java
@@ -30,7 +30,7 @@ public abstract class AbstractTest {
         }
         ctx.setAttribute("driver", this.driver);
     }
-    // Access token - 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
+    // Access token
     private WebDriver getLocaldriver() {
     	return new ChromeDriver();
     }
